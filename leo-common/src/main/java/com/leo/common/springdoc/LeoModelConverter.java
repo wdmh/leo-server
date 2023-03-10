4dcc3ccd4c83e26e00e89d5e2fdccc07f6ef0e0e
lewis <liujie1861@gmail.com>
2023-03-10 09:38:22 +0800

    feat: 修改springdoc定制显示

--- leo-common/src/main/java/com/leo/common/springdoc/LeoModelConverter.java
+++ leo-common/src/main/java/com/leo/common/springdoc/LeoModelConverter.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.leo.common.base.LeoEnum;
+import com.leo.util.StringUtils;
 import io.swagger.v3.core.converter.AnnotatedType;
 import io.swagger.v3.core.converter.ModelConverter;
 import io.swagger.v3.core.converter.ModelConverterContext;
@@ -16,11 +17,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.lang.annotation.Annotation;
+import java.time.temporal.Temporal;
 import java.util.Iterator;
 
 /**
- * 定制LeoEnum swagger接口文档显示参数说明，返回{"code": 0,"text": "未知"}形式
- * 通过重写Schema实现类应该也可以达到同样的效果，但是并未测试
+ * 定制swagger接口文档显示参数说明，
+ * 1. LeoEnum 返回{"code": 0,"text": "未知"}形式
+ * 2. Temporal类显示，重写
  *
  * @author liujie
  */
@@ -28,37 +31,49 @@ import java.util.Iterator;
 @Component
 @RequiredArgsConstructor
 @SuppressWarnings("unchecked")
-public class LeoEnumModelConverter implements ModelConverter {
+public class LeoModelConverter implements ModelConverter {
 
     private final ObjectMapper objectMapper;
 
     @Override
     public Schema resolve(AnnotatedType type, ModelConverterContext context, Iterator<ModelConverter> chain) {
         JavaType javaType = objectMapper.constructType(type.getType());
+
+        StringSchema schema = new StringSchema();
+        // 设置LeoEnum枚举类
         if (javaType != null && javaType.isEnumType() && LeoEnum.class.isAssignableFrom(javaType.getRawClass())) {
             Class<LeoEnum> enumClass = (Class<LeoEnum>) javaType.getRawClass();
-            if (LeoEnum.class.isAssignableFrom(enumClass)) {
-                StringSchema schema = new StringSchema();
-                try {
-                    LeoEnum[] enumConstants = enumClass.getEnumConstants();
-                    for (LeoEnum leoEnum : enumConstants) {
-                        String msg = objectMapper.writeValueAsString(leoEnum);
-                        schema.addEnumItem(msg);
-                    }
-                    setLeoEnumSchemaInfo(type, javaType, schema);
-                } catch (JsonProcessingException e) {
-                    log.error("LeoEnum生成Api文档转换异常", e);
+            LeoEnum[] enumConstants = enumClass.getEnumConstants();
+            try {
+                for (LeoEnum leoEnum : enumConstants) {
+                    String msg = objectMapper.writeValueAsString(leoEnum);
+                    schema.addEnumItem(msg);
                 }
-                return schema;
+                setLeoEnumSchemaInfo(type, javaType, schema);
+            } catch (JsonProcessingException e) {
+                log.error("LeoEnum生成Api文档转换异常", e);
             }
+            return schema;
         }
+
+        // 设置Temporal举类
+        if (javaType != null && Temporal.class.isAssignableFrom(javaType.getRawClass())) {
+            StringSchema stringSchema = new StringSchema();
+            try {
+                setLeoEnumSchemaInfo(type, javaType, stringSchema);
+            } catch (JsonProcessingException e) {
+                log.error("LeoEnum生成Api文档转换异常", e);
+            }
+            return stringSchema;
+        }
+
         return chain.hasNext() ? chain.next().resolve(type, context, chain) : null;
     }
 
 
     /**
      * 重写Schema信息
-     * 因为针对LeoEnum信息展示进行了重写，故重新填写@Schema内example信息,如有其他需要展示，也可继续扩展
+     * 因为针对信息展示进行了重写，故重新填写@Schema内信息,如有其他需要展示，也可继续扩展
      *
      * @param type     AnnotatedType
      * @param javaType JavaType
@@ -69,9 +84,23 @@ public class LeoEnumModelConverter implements ModelConverter {
         Annotation resolvedSchemaOrArrayAnnotation = AnnotationsUtils.mergeSchemaAnnotations(type.getCtxAnnotations(), javaType);
         io.swagger.v3.oas.annotations.media.Schema schemaAnnotation = resolvedSchemaOrArrayAnnotation == null ? null : (resolvedSchemaOrArrayAnnotation instanceof ArraySchema ? ((ArraySchema) resolvedSchemaOrArrayAnnotation).schema() : (io.swagger.v3.oas.annotations.media.Schema) resolvedSchemaOrArrayAnnotation);
 
-        // 设置schema example示例值
-        if (schemaAnnotation != null && !schemaAnnotation.example().isEmpty()) {
-            schema.example(objectMapper.readTree(schemaAnnotation.example()));
+        if (schemaAnnotation == null) {
+            return;
+        }
+
+        // 设置schema description 值
+        if (StringUtils.isNotBlank(schemaAnnotation.description())) {
+            schema.description(schemaAnnotation.description());
+        }
+
+        // 设置schema example 值
+        if (StringUtils.isNotBlank(schemaAnnotation.example())) {
+            schema.example(schemaAnnotation.example());
+        }
+
+        // 设置schema defaultValue 值
+        if (StringUtils.isNotBlank(schemaAnnotation.defaultValue())) {
+            schema.setDefault(schemaAnnotation.defaultValue());
         }
     }
 }
